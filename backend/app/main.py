deeb156a1ff18909f97fe2924b252d059c4d5b5c
Nihar Shah <nihar.shah100905@gmail.com>
2026-05-11 02:43:54 +0530

    temp open CORS for debugging

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://querymind-io.netlify.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
